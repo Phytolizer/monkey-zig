23e3cb2173392f3320da2c1e0717002136c49825
Kyle Coffey <me@phytolizer.dev>
2022-08-17 16:28:01 -0500

    unhardcode the C stuff

--- src/unix.zig
+++ src/unix.zig
@@ -1,24 +1,14 @@
-pub const _SC_GETPW_R_SIZE_MAX: c_int = 70;
-pub extern "c" fn sysconf(sc: c_int) i64;
+const c = @cImport({
+    @cInclude("unistd.h");
+    @cInclude("sys/types.h");
+    @cInclude("pwd.h");
+});
+pub const _SC_GETPW_R_SIZE_MAX = c._SC_GETPW_R_SIZE_MAX;
+pub const sysconf = c.sysconf;
 
-pub extern "c" fn getuid() uid_t;
+pub const getuid = c.getuid;
 
-pub const uid_t = c_uint;
-pub const gid_t = c_uint;
-pub const passwd = extern struct {
-    pw_name: [*c]u8,
-    pw_passwd: [*c]u8,
-    pw_uid: uid_t,
-    pw_gid: gid_t,
-    pw_gecos: [*c]u8,
-    pw_dir: [*c]u8,
-    pw_shell: [*c]u8,
-};
-
-pub extern "c" fn getpwuid_r(
-    uid: uid_t,
-    pwd: [*c]passwd,
-    buf: [*c]u8,
-    buflen: usize,
-    result: [*c][*c]passwd,
-) c_int;
+pub const uid_t = c.uid_t;
+pub const gid_t = c.gid_t;
+pub const passwd = c.passwd;
+pub const getpwuid_r = c.getpwuid_r;
